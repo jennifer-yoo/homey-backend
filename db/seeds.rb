ea1b5534bd6579d1eb39481f2c0a9a04daf93dd7
jennifer-yoo <jennifer.yoo7@gmail.com>
2020-10-06 23:28:34 -0400

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Order.destroy_all
+Cart.destroy_all
+Furniture.destroy_all
+
+
+User.create(username: "jennyoyo", password: "1234")
+
+Furniture.create(room: "Living Room", category: "Sofa", price: 250, name: "The Bob", color: "Black", rating: 0, image: "https://freepngimg.com/thumb/sofa/7-sofa-png-image.png")
+Furniture.create(room: "Living Room", category: "Sofa", price: 480, name: "Fairydust", color: "Cream", rating: 0, image: "https://freepngimg.com/thumb/sofa/17-sofa-png-image.png")
+Furniture.create(room: "Living Room", category: "Sofa", price: 300, name: "Elegancy", color: "Olive", rating: 0, image: "https://freepngimg.com/thumb/armchair/48-armchair-png-image.png")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+# Furniture.create(room: "", category: "", price: , name: "", color: "", rating: 0, image: "")
+
+puts "#{Furniture.all.count} items seeds"
